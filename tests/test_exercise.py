6bc641a7ad5e045f01a34bcba747c6a3a9ac632f
David Ham <David.Ham@imperial.ac.uk>
2020-09-14 13:06:59 +0100

    Initial toy exercise

---
+++ tests/test_exercise.py
@@ -0,0 +1,8 @@
+import os
+
+def test_exercise():
+
+    with open(os.path.dirname(__file__) + "/../exercise.txt", "r") as f:
+        contents = f.read()
+        if contents.split() != ("Hello", "Mars!"):
+            raise ValueError(f"Expected 'Hello Mars!', got '{contents.strip()}'")
