b53688956dad569999bcd017734c12792475bad4
David Ham <David.Ham@imperial.ac.uk>
2020-09-14 13:12:06 +0100

    Fix test

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -4,5 +4,5 @@ def test_exercise():
 
     with open(os.path.dirname(__file__) + "/../exercise.txt", "r") as f:
         contents = f.read()
-        if contents.split() != ("Hello", "Mars!"):
+        if contents.split() != ["Hello", "Mars!"]:
             raise ValueError(f"Expected 'Hello Mars!', got '{contents.strip()}'")
